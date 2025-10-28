c39e089c0f93b63aebd8238cd41bd7fbe2c94fd1
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-10-28 14:28:25 +0700

    Membuat komik,js

---
+++ models/komik.js
@@ -0,0 +1,27 @@
+module.export = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+            penulis: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    },{
+        tableName: 'komik',
+        timestamps: false, 
+        freezeTableName: true
+    
+    });
+    return Komik;
+}
